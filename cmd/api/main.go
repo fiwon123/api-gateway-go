057cd6765959afd4bc5d9ac72160fb7af0d218de
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-09-19 15:58:27 -0300

    implement serve

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	})
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+
+	log.Println("API gateway listening on http://localhost:8080")
+	
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+
 	
 }
